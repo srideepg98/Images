c96da95051ac5bc33a3ed21ab82dc4acb13f7263
srideepg98 <srideepanshu24@gmail.com>
2019-04-12 23:47:49 +0530

    Created

---
+++ record.py
@@ -0,0 +1,15 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    rect, frame = cap.read()
+    
+    cv2.imshow('frame', frame)
+
+
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+    		break
+
+cap.release()
+cv2.destroyAllWindows()
